2257749caa5c1dc094d33303536ef0d276125761
Amrrish1209 <roshanbabubl@gmail.com>
2023-03-21 19:13:19 +0530

    Add: As a fan of geometry, I want to check equality of two lines based on the end points, So that I know when two lines are the equal.

--- src/com/bridgelabz/linecomparisonprogram/LineComparison.java
+++ src/com/bridgelabz/linecomparisonprogram/LineComparison.java
@@ -3,17 +3,27 @@ package com.bridgelabz.linecomparisonprogram;
 public class LineComparison {
 
 	public static void main(String[] args) {
-
+		
+		//Enter the co-ordinates of point 1 of (x1,y1) of line 1
 		double x1 = 2.0;
 		double y1 = 3.0;
+		//Enter the co-ordinates of point 2 of (x2,y2) of line 1
 		double x2 = 4.0;
 		double y2 = 7.0;
-
+		
+		//Enter the co-ordinates of point 1 (x3,y3) of line 2
+		double x3=3.0;
+		double y3=2.0;
+		//Enter the co-ordinates of point 2(x4,y4) of line 2
+		double x4=7.0;
+		double y4=4.0;
 		// calculate the length of the line segment
-		double length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+		double lengthOne = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+		double lengthTwo = Math.sqrt(Math.pow(x4 - x3, 2) + Math.pow(y4 - y3, 2));
 
 		// print out the result
-		System.out.println("The length of the line segment is: " + length);
+		System.out.println("The length of the line segment of length one is: " + lengthOne);
+		System.out.println("The length of the line segment of length two is: " + lengthTwo);
 
 	}
 }
