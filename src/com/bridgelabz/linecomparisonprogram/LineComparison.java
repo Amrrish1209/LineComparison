4994f266689ee3b721a380da7256f2be489f1512
Amrrish1209 <roshanbabubl@gmail.com>
2023-03-28 14:24:09 +0530

    Add: Use Java Object Oriented Programming Concepts of Line and Point as well as equals and compareTo methods.

--- src/com/bridgelabz/linecomparisonprogram/LineComparison.java
+++ src/com/bridgelabz/linecomparisonprogram/LineComparison.java
@@ -2,36 +2,28 @@ package com.bridgelabz.linecomparisonprogram;
 
 public class LineComparison {
 
-	public static void main(String[] args) {
-		
-		//Enter the co-ordinates of point 1 of (x1,y1) of line 1
-		double x1 = 2.0;
-		double y1 = 3.0;
-		//Enter the co-ordinates of point 2 of (x2,y2) of line 1
-		double x2 = 4.0;
-		double y2 = 7.0;
-		
-		//Enter the co-ordinates of point 1 (x3,y3) of line 2
-		double x3=3.0;
-		double y3=2.0;
-		//Enter the co-ordinates of point 2(x4,y4) of line 2
-		double x4=7.0;
-		double y4=4.0;
-		
-		// calculate the length of the line segment
+	public static final double x1 = 2.0;
+	public static final double y1 = 3.0;
+	public static final double x2 = 4.0;
+	public static final double y2 = 7.0;
+	public static final double x3 = 7.0;
+	public static final double y3 = 4.0;
+	public static final double x4 = 3.0;
+	public static final double y4 = 2.0;
+
+	public static void calculate() {
 		double lengthOne = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
 		double lengthTwo = Math.sqrt(Math.pow(x4 - x3, 2) + Math.pow(y4 - y3, 2));
-		
-		// compare the lengths of the lines
 		if (lengthOne == lengthTwo) {
 			System.out.println("The length of line one is equal to the length of line two");
-		}
-		else if (lengthOne > lengthTwo) {
+		} else if (lengthOne > lengthTwo) {
 			System.out.println("The length of line one is greater than the length of line two");
-		}
-		else {
+		} else {
 			System.out.println("The length of line one is less than the length of line two");
 		}
+	}
 
+	public static void main(String[] args) {
+		LineComparison.calculate();
 	}
 }
